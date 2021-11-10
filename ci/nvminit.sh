35d0adbca689b864201d53689b08c76f1f421fa8
Jim Hollington <jim@bozboz.co.uk>
2021-11-10 16:11:18 +0000

    Starting to think I might just abandon having nvm installed in this image

--- ci/nvminit.sh
+++ ci/nvminit.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-source /home/php/.bashrc
-fn_exists() {
-  # appended double quote is an ugly trick to make sure we do get a string -- if $1 is not a known command, type does not output anything
-  [ `type -t $1`"" == 'function' ]
-}
-
-if ! fn_exists "nvm"; then
-  NVM_DIR="$HOME/.nvm"
-  source "$NVM_DIR/nvm.sh" # This loads nvm
-fi
-if test -f ".nvmrc"; then nvm use || nvm install; fi
 # shellcheck disable=SC2068
+source /home/php/.bashrc
+
+NVM_DIR="/home/php/.nvm"
+source "$NVM_DIR/nvm.sh"
+
+nvm_auto install
 nvm ${@:1}
